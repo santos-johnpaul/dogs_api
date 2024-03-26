bb9b857252584b45504e6d2634720bd1d04f2982
thirdyyy <thirdcao15@gmail.com>
2024-03-26 09:19:22 +0800

    Santiago Create stateful and initstate

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class DogImagesPage extends StatefulWidget {
+  @override
+  _DogImagesPageState createState() => _DogImagesPageState();
+}
+
+class _DogImagesPageState extends State<DogImagesPage> {
+  List<String> _dogImageUrls = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDogImages();
+  }
